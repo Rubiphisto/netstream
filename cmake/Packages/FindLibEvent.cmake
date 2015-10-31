34c0fcaa89d5be158fccc4b750292ab74a64db98
Allen Young <snwcwt@gmail.com>
2015-10-31 13:36:27 +0800

    compiled under VS2015

--- cmake/Packages/FindLibEvent.cmake
+++ cmake/Packages/FindLibEvent.cmake
@@ -14,10 +14,16 @@ endif(MSVC)
 if(NOT LibEvent_Build AND LibEvent_Source)
 	set(LibEvent_Build ${LibEvent_Source} ${LibEvent_Source}/lib )
 endif()
-find_library(LibEvent_LIB_Basis NAMES event PATHS ${LibEvent_Build})
-find_library(LibEvent_LIB_Core NAMES event_core PATHS ${LibEvent_Build})
-find_library(LibEvent_LIB_Extras NAMES event_extra PATHS ${LibEvent_Build})
-find_library(LibEvent_LIB_Pthreads NAMES event_pthreads PATHS ${LibEvent_Build})
+if( MSVC )
+	find_library(LibEvent_LIB_Basis NAMES libevent PATHS ${LibEvent_Build})
+	find_library(LibEvent_LIB_Core NAMES libevent_core PATHS ${LibEvent_Build})
+	find_library(LibEvent_LIB_Extras NAMES libevent_extras PATHS ${LibEvent_Build})
+else()
+	find_library(LibEvent_LIB_Basis NAMES event PATHS ${LibEvent_Build})
+	find_library(LibEvent_LIB_Core NAMES event_core PATHS ${LibEvent_Build})
+	find_library(LibEvent_LIB_Extras NAMES event_extra PATHS ${LibEvent_Build})
+	find_library(LibEvent_LIB_Pthreads NAMES event_pthreads PATHS ${LibEvent_Build})
+endif()
 
 if (LibEvent_LIB_Basis AND LibEvent_INCLUDE_DIR)
 	set(LibEvent_FOUND TRUE)
@@ -35,10 +41,20 @@ else ()
 	message(STATUS "libevent NOT found.")
 endif ()
 
-mark_as_advanced(
-	LibEvent_INCLUDE_DIR
-	LibEvent_LIB_Basis
-	LibEvent_LIB_Core
-	LibEvent_LIB_Extras
-	LibEvent_LIB_Pthreads
-)
+if( MSVC )
+	mark_as_advanced(
+		LibEvent_INCLUDE_DIR
+		LibEvent_LIB_Basis
+		LibEvent_LIB_Core
+		LibEvent_LIB_Extras
+	)
+else()
+	mark_as_advanced(
+		LibEvent_INCLUDE_DIR
+		LibEvent_LIB_Basis
+		LibEvent_LIB_Core
+		LibEvent_LIB_Extras
+		LibEvent_LIB_Pthreads
+	)
+endif()
+
